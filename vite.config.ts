5481e50f66d188219003d88a19593595fbda5964
stretchmame <64906130+stretchmame@users.noreply.github.com>
2026-03-23 14:24:12 +0800

    Update vite.config.ts
    base: './'

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
-
-export default defineConfig(({mode}) => {
+base: './'
+  export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
